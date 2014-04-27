c2c9dfc40af9c1a133fc1a4873734ca6ddcc8f29
Pilen <fiskomaten@gmail.com>
2014-04-27 15:53:24 +0200

    working on email obfuscation script

--- public/functions.js
+++ public/functions.js
@@ -2,7 +2,7 @@
 function replaceEmail (divClass, address) {
     var emails = document.getElementsByClassName(divClass);
     for (var i = 0; i < emails.length; i++) {
-        var enc = s.replace(/./g, function(letter) {return String.fromCharCode(letter.charCodeAt(0)+30)});
+        var enc = address.replace(/./g, function(letter) {return String.fromCharCode(letter.charCodeAt(0)+30)});
         emails[i].innerHTML = enc
     }
 }
