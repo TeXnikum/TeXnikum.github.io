947755c724390119dce72a3f080652350ce034dd
Pilen <fiskomaten@gmail.com>
2014-04-27 15:12:31 +0200

    added functions.js

---
+++ public/functions.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    replaceEmail('email-list', 'WUGT"GZCORNGEQO');
+    replaceEmail('email-admin', 'WUGT"GZCORNGEQO');
+}
+
+function replaceEmail (divClass, address) {
+    var emails = document.getElementsByClassName(divClass);
+    for (var i = 0; i < emails.length; i++) {
+        var enc = s.replace(/./g, function(letter) {return String.fromCharCode(letter.charCodeAt(0)+30)});
+        emails[i].innerHTML = enc
+    }
+}
